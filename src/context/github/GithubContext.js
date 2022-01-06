9d7780c94e4fe4cc7fb657a7f1d4587390259396
Farshad <farshad.ghanbari@hotmail.com>
2022-01-06 09:00:54 +0800

    added user profile stats

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
     loading: false,
   }
 
@@ -30,6 +31,23 @@ export const GithubProvider = ({ children }) => {
     dispatch({ type: 'GET_USERS', payload: items })
   }
 
+  const getUser = async (login) => {
+    setLoading()
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    })
+
+    if (response.status === 404) {
+      window.location = '/notfound'
+    } else {
+      const data = await response.json()
+      dispatch({ type: 'GET_USER', payload: data })
+    }
+  }
+
   const clearUsers = () => {
     dispatch({ type: 'CLEAR_USERS' })
   }
@@ -42,9 +60,11 @@ export const GithubProvider = ({ children }) => {
     <GithubContext.Provider
       value={{
         users: state.users,
+        user: state.user,
         loading: state.loading,
         searchUsers,
         clearUsers,
+        getUser,
       }}>
       {children}
     </GithubContext.Provider>
